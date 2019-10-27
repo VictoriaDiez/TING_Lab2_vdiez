0149645f3637139e319da797dd9720680b59939e
Victoria Díez Acereda <victoriadiezacereda@MacBook-Pro-de-Victoria.local>
2019-10-27 14:40:48 +0000

    version 1

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Tecnologías de Internet de Nueva Generación del 2019' });
 });
 
 module.exports = router;
