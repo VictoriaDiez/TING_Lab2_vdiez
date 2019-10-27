db268b4806e749809f7495ad6069390dea5d4478
Victoria Díez Acereda <victoriadiezacereda@MacBook-Pro-de-Victoria.local>
2019-10-27 16:10:22 +0000

    version 2

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const { Pool } = require ('pg');
+const pool = new Pool ({
+	connectionString: 'postgres://iftnibitynhxah:7b444979be7d601379e0252c638a83cdc548a4b99e7352df14aaa2fa55e19838@ec2-174-129-253-101.compute-1.amazonaws.com:5432/d1atd9iho2kb5f'
+	ssl: true
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Tecnologías de Internet de Nueva Generación del 2019' });
 });
+
+router.get( '/db', async ( req, res ) => {
+	try{
+		const client = await pool.connect()
+		const result = await client.query( 'SELECT * FROM instrumentos_musica' );
+		const results = { 'results': ( result ) ? result.rows: null};
+		res.render( 'db', results );
+		client.release();
+	} catch ( err ) {
+		console.error ( err );
+		res.send(" Error " + err);
+	}
+})
 
 module.exports = router;
