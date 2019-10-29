d2de427410c9df643c108fa4b1c39ca0e5319507
Victoria Díez Acereda <victoriadiezacereda@MacBook-Pro-de-Victoria.local>
2019-10-29 19:57:30 +0000

    version 3

--- routes/index.js
+++ routes/index.js
@@ -24,4 +24,17 @@ router.get( '/db', async ( req, res ) => {
 	}
 })
 
+router.get( '/db', async ( req, res ) => {
+	try{
+		const client = await pool.connect();
+		const result = await client.query( 'SELECT * FROM instrumentos_musica' );
+		const results = { 'results': ( result ) ? result.rows: 1};
+		res.render( 'db', results );
+		client.release();
+	} catch ( err ) {
+		console.error ( err );
+		res.send(" Error " + err);
+	}
+})
+
 module.exports = router;
